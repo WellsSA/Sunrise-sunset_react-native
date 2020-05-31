747c5d699d4fcb206fa8f9323e23b9de1b835063
WellsSA <wel.cavzod@gmail.com>
2020-05-31 10:48:43 -0300

    feature(): api service definition && first api call

--- App.js
+++ App.js
@@ -1,22 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 import { OPEN_WEATHER_API_KEY } from 'react-native-dotenv';
+import api from './services/api';
+import axios from 'axios';
 
 export default function App() {
+  const [city, setCity] = useState('curitiba');
+
+  const callApi = async () => {
+    if (!city) return;
+    try {
+      const { data } = await api.get('/find', {
+        params: {
+          q: city,
+          appid: OPEN_WEATHER_API_KEY,
+        },
+      });
+
+      if (!data.list.length) throw new Error('City not found');
+
+      const [
+        {
+          coord: { lat, lon },
+        },
+      ] = data.list;
+
+      console.log({ lat, lon });
+    } catch (error) {
+      Alert.alert('An error occurred', error.message, [
+        {
+          text: 'OK',
+          style: 'default',
+        },
+      ]);
+      console.log({ error });
+    }
+
+    // const data = await axios.get('https://pokeapi.co/api/v2');
+
+    // const data = await api.get('/find', {
+    //   query: {
+    //     q: city,
+    //     appid: OPEN_WEATHER_API_KEY,
+    //   },
+    // });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app! {OPEN_WEATHER_API_KEY}
-      </Text>
+      <View style={styles.inputGroup}>
+        <TextInput
+          style={styles.cityNameInput}
+          placeholder="Digite o nome de uma cidade"
+          value={city}
+          onChangeText={setCity}
+        />
+        <Button title="Ok" style={styles.confirmButton} onPress={callApi} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  cityNameInput: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9,
+  },
+  confirmButton: {
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
   },
 });
