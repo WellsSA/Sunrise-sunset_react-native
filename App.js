8fd9826c017f8dd2b5cb1fb53409646dfe39f2f3
WellsSA <wel.cavzod@gmail.com>
2020-05-31 10:17:05 -0300

    feature(): setting up environment variables

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { OPEN_WEATHER_API_KEY } from 'react-native-dotenv';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Open up App.js to start working on your app! {OPEN_WEATHER_API_KEY}
+      </Text>
     </View>
   );
 }
